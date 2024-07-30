a5958ccd41ff5f366265de971a4e3bff81062fe1
Dylan632 <165628762+Dylan632@users.noreply.github.com>
2024-07-30 10:24:50 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,8 +12,8 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: '我的跑步日志',
   siteUrl: 'https://dylan632.github.io/running_page/',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  logo: 'https://github.com/Dylan632/running_page/blob/master/logo.jpeg',
+  description: 'Personal site',
   navLinks: [
     {
       name: 'Blog',
