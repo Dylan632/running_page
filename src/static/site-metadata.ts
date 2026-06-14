880c5812a33f6a68f0892ba6ea5975a743a88a5c
Dylan632 <xxx@xxx.com>
2026-06-14 14:56:54 +0800

    Align production with Kami parchment style

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -15,7 +15,7 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: '我的跑步日志',
+  siteTitle: 'Running Page',
   siteUrl: 'https://dylan632.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site',
